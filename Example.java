cf2ba1815cd0ae793914a7577246c7ccd0408285
ganapathi190 <86883550+ganapathi190@users.noreply.github.com>
2021-07-03 20:06:57 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,19 @@
+
+package practice;
+
+import java.util.*;
+import java.lang.*;
+
+public class Example {
+int x=10;
+static int y= 20;
+ int z = 12;
+
+static void st() {
+	System.out.println("static suc");
+}
+
+final void fi() {
+	System.out.println("final suc");
+}
+}
